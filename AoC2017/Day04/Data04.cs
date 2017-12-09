b80b79b9302c3856333cd3bced6583f4069700e0
Daniel Pettersson <danpett89@gmail.com>
2017-12-09 11:17:57 +0100

    Solved day 4.

---
+++ AoC2017/Day04/Data04.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AoC2017.Day04
+{
+   public class Data04
+   {
+      public IEnumerable<string> Part1()
+      {
+         var path = @"C:\Users\Administrator\Source\Repos\AdventOfCode\AoC2017\Day04\Input04.txt";
+         return GetFileByLine(path);
+      }
+
+      public string Part2()
+      {
+         return string.Empty;
+      }
+
+      private static IEnumerable<string> GetFileByLine(string path)
+      {
+         using (var file = new StreamReader(path))
+         {
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+               yield return line;
+            }
+         }
+      }
+   }
+}
---
+++ AoC2017/Day04/Problem04.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AoC;
+
+namespace AoC2017.Day04
+{
+   public class Problem04 : IProblem
+   {
+      private readonly List<string> _input = new Data04().Part1().ToList();
+
+      public string SolvePart1()
+      {
+         return _input.Count(HasUniqueWords).ToString();
+      }
+
+      public string SolvePart2()
+      {
+         return _input.Count(HasNoAnagrams).ToString();
+      }
+
+      private static bool HasUniqueWords(string passPhrase)
+      {
+         return passPhrase.Split(' ').Unique();
+      }
+
+      private static bool HasNoAnagrams(string passPhrase)
+      {
+         return !passPhrase.Split(' ').ToList().WhereIntersect(IsAnagram).Any();
+      }
+
+      private static bool IsAnagram(string s1, string s2)
+      {
+         return s1.OrderBy(c => c).SequenceEqual(s2.OrderBy(c => c));
+      }
+   }
+}
