928f957ccb8d17669aed512fa62694ba77266120
Daniel Pettersson <danpett89@gmail.com>
2017-12-09 11:15:17 +0100

    Added some extensions.

--- AoC/EnumerableExtensions.cs
+++ AoC/EnumerableExtensions.cs
@@ -21,5 +21,26 @@ namespace AoC
          int i = 0;
          foreach (var t in @this) action(i++, t);
       }
+
+      public static bool Unique<T>(this IEnumerable<T> @this)
+      {
+         return @this.GroupBy(v => v).All(c => c.Count() == 1);
+      }
+
+      public static IEnumerable<Tuple<T, T>> WhereIntersect<T>(this List<T> @this, Func<T, T, bool> pred)
+      {
+         for (int i = 0; i < @this.Count - 1; ++i)
+         {
+            for (int j = i + 1; j < @this.Count; ++j)
+            {
+               if (pred(@this[i], @this[j])) yield return new Tuple<T, T>(@this[i], @this[j]);
+            }
+         }
+      }
+
+      public static IEnumerable<List<T>> Split<T>(this List<T> @this, int chunkSize)
+      {
+         for (int i = 0; i < @this.Count; i += chunkSize) yield return @this.Skip(i).Take(chunkSize).ToList();
+      }
    }
 }
